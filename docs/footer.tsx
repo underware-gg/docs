a041b1222e753358a5d5f62e6f026b78c9a21b7f
R13 <rbentos@yahoo.com>
2024-05-27 21:49:58 -0300

    basic structure

---
+++ docs/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <div>
+      {/* <div>Released under the MIT License.</div> */}
+      <div>Copyright © 2023-present Underware.gg</div>
+    </div>
+  )
+}
