142807ab4340322a8c94c7d4a89add214c68cd22
R13 <rbentos@yahoo.com>
2024-05-28 18:23:16 -0300

    logos on all main pages

--- docs/components/Logo.tsx
+++ docs/components/Logo.tsx
@@ -2,24 +2,54 @@
 // import { Button } from "vocs/components"
 
 type LogoProps = {
+  src?: string
+  // size
   small?: boolean
   big?: boolean
-  src?: string
+  // align
+  center?: boolean
+  left?: boolean
+  right?: boolean
 }
 
 function Logo(props: LogoProps) {
-  let classNames:string[] = []
-  if (props.small) classNames.push('LogoSmall')
-  else if (props.big) classNames.push('LogoBig')
-  else classNames.push('Logo')
-  return <img src={props.src} className={classNames.join(' ')}/>
+  let divClasses:string[] = ['flex']
+  if (props.center) divClasses.push('justify-center')
+  else if (props.right) divClasses.push('justify-right')
+  else if (props.left) divClasses.push('justify-left')
+  
+  let imgClasses: string[] = []
+  if (props.small) imgClasses.push('LogoSmall')
+  else if (props.big) imgClasses.push('LogoBig')
+  else imgClasses.push('Logo')
+  
+  return (
+    <div className={divClasses.join(' ')}>
+      <img src={props.src} className={imgClasses.join(' ')} />
+    </div>
+  )
 }
 
 function Underware(props: LogoProps) {
   return <Logo {...props} src="/images/logo_underware.png" />
 }
 
+function Pistols(props: LogoProps) {
+  return <Logo {...props} src="/images/logo_pistols.png" />
+}
+
+function Underdark(props: LogoProps) {
+  return <Logo {...props} src="/images/logo_underdark.png" />
+}
+
+function Underworld(props: LogoProps) {
+  return <Logo {...props} src="/images/logo_underworld.png" />
+}
+
 export default {
   Logo,
   Underware,
+  Pistols,
+  Underdark,
+  Underworld,
 }
